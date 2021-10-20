623939b8b2b27c8dd9bda857725216c605356f49
Murat Menzilci <menzilcim>
2021-10-20 15:22:55 +0300

    Add Detail and Entry View Controllers

---
+++ toDoListWithRealm/EntryViewController.swift
@@ -0,0 +1,22 @@
+//
+//  EntryViewController.swift
+//  toDoListWithRealm
+//
+//  Created by Murat Menzilci on 20.10.2021.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
+
+}
