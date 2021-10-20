0f11968f2d09f77f29483c425f0bfc1de5098b64
Murat Menzilci <menzilcim>
2021-10-20 11:56:39 +0300

    Initial Commit

---
+++ toDoListWithRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoListWithRealm
+//
+//  Created by Murat Menzilci on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
